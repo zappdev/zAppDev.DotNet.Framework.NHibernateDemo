f5330f40c4201dc07bb4246a1b23e58940432ca1
Bill <bill_vasilopoulos@hotmail.com>
2019-09-20 17:06:26 +0300

    Functionality added

--- NHibernateDemo/NHibernateDemo/ClientApp/src/app/Components/Teams/teams-list/teams-list.component.ts
+++ NHibernateDemo/NHibernateDemo/ClientApp/src/app/Components/Teams/teams-list/teams-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Team } from '../../../Models/Team';
+import { TeamsService } from '../../../Services/teams.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-teams-list',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamsListComponent implements OnInit {
 
-  constructor() { }
+  dataSource: Team[];
+  displayedColumns = ['id', 'name', 'founded','city' , 'actions'];
+
+  constructor(private _teamService: TeamsService, private _router: Router) { }
 
   ngOnInit() {
   }
 
+  loadTeams() {
+    this._teamService.getTeams().pipe()
+      .subscribe(
+        (data: any) => {
+          this.dataSource = data.body.value;
+        }
+      );
+  }
+
+  deleteTeam(team: Team) {
+    this._teamService.deleteTeam(team.id).subscribe(
+      () => {
+        this.loadTeams();
+      }
+    );
+  }
+
+  editTeam(row) {
+    this._router.navigate(['/team-edit', row.id]);
+  }
+
+  addTeam() {
+    this._router.navigate(['/team-add']);
+  }
 }
--- NHibernateDemo/NHibernateDemo/ClientApp/src/app/app.module.ts
+++ NHibernateDemo/NHibernateDemo/ClientApp/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +10,10 @@ import { PlayersListComponent } from './Components/Players/players-list/players-
 import { PlayersDetailsComponent } from './Components/Players/players-details/players-details.component';
 import { TeamsListComponent } from './Components/Teams/teams-list/teams-list.component';
 import { TeamsDetailsComponent } from './Components/Teams/teams-details/teams-details.component';
+import { MatNativeDateModule, MatGridListModule, MatFormFieldModule, MatListModule, MatCardModule, MatTableModule, MatIconModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatSelectModule, MatCheckboxModule } from '@angular/material';
+import { MatMomentDateModule, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -26,17 +29,33 @@ import { TeamsDetailsComponent } from './Components/Teams/teams-details/teams-de
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    MatNativeDateModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatMomentDateModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: PlayersListComponent, pathMatch: 'full' },
       { path: 'players', component: PlayersListComponent, pathMatch: 'full' },
-      { path: 'players-add', component: PlayersDetailsComponent, pathMatch: 'full' },
-      { path: 'players-edit', component: PlayersDetailsComponent, pathMatch: 'full' },
+      { path: 'player-add', component: PlayersDetailsComponent, pathMatch: 'full' },
+      { path: 'player-edit/:id', component: PlayersDetailsComponent, pathMatch: 'full' },
       { path: 'teams', component: TeamsListComponent, pathMatch: 'full' },
-      { path: 'teams-add', component: TeamsDetailsComponent, pathMatch: 'full' },
-      { path: 'teams-add', component: TeamsDetailsComponent, pathMatch: 'full' }
+      { path: 'team-add', component: TeamsDetailsComponent, pathMatch: 'full' },
+      { path: 'team-edit/:id', component: TeamsDetailsComponent, pathMatch: 'full' }
     ])
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
